032b40bfaae8b72c10b3be4dba737f6d5720bb8b
Comparitiko <gabigcy@gmail.com>
2024-04-15 02:23:20 +0200

    Finalizada configuracion

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,44 +1,65 @@
-/** @type {import("eslint").Linter.Config} */
+process.env.ESLINT_TSCONFIG = 'tsconfig.json'
+
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 module.exports = {
-  extends: ['plugin:astro/recommended', "eslint-config-prettier"],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-    ecmaVersion: 'latest'
-  },
-  overrides: [
-    {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
-      },
-      rules: {
-        "no-unused-vars": "error",
-        "quotes": ["error", "single"],
-		    "semi": ["error", "never"],
-        "space-before-function-paren": ["error", "never"],
-        "@typescript-eslint/no-unused-vars": "off",
-        "unused-imports/no-unused-imports": "error",
-        "object-curly-newline": [
-          "warn",
-          {
-            consistent: true,
-            multiline: true,
-          },
-        ],
-        "object-curly-spacing": ["warn", "always"],
-        "array-element-newline": ["warn", "consistent"],
-        "array-bracket-newline": ["warn", "consistent"],
-      },
-    },
-    {
+	globals: {
+		NodeJS: true,
+		NodeListOf: true,
+	},
+	env: {
+		es2022: true,
+		node: true,
+		browser: true,
+	},
+	extends: ['eslint-config-prettier'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'quotes': ['warn', 'single'],
+		'semi': ['warn', 'never'],
+		'no-trailing-spaces': 'warn',
+		'space-before-function-paren': ['error', 'always'],
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'object-curly-newline': [
+			'warn',
+			{
+				consistent: true,
+				multiline: true,
+			},
+		],
+		'object-curly-spacing': ['warn', 'always'],
+		'array-element-newline': ['warn', 'consistent'],
+		'array-bracket-newline': ['warn', 'consistent'],
+	},
+	overrides: [
+		{
+			files: ['*.astro', 'src/utils/serviceWorker.ts'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
+			},
+			extends: ['plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended'],
+			rules: {
+				'astro/no-conflict-set-directives': 'warn',
+				'astro/no-unused-define-vars-in-style': 'warn',
+				'astro/no-unused-css-selector': 'off',
+				'astro/prefer-class-list-directive': 'warn',
+				'astro/semi': ['warn', 'never'],
+				'astro/jsx-a11y/anchor-is-valid': 'warn',
+			},
+		},
+		{
 			// Define the configuration for `<script>` tag.
 			// Script in `<script>` is assigned a virtual file name with the `.js` extension.
-			files: ["**/*.astro/*.js", "*.astro/*.js"],
-			parser: "@typescript-eslint/parser",
+			files: ['**/*.astro/*.js', '*.astro/*.js'],
+			parser: '@typescript-eslint/parser',
 		},
-  ]
+	],
 }
